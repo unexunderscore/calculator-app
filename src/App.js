972654260bb38fc2ba4bf64139b19f504f58971d
Attila Tóth <toth_attila@outlook.hu>
2022-04-06 16:31:07 +0200

    Remodified math logic, with simple one and add radio buttons with simple style, will change it later

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 
 function App() {
 
   const [number, setNumber] = useState('');
 
-  
+  const [checked, setChecked] = useState(() => {
+    // getting stored value
+
+      const saved = localStorage.getItem("checked");
+      const initialValue = JSON.parse(saved);
+      if(initialValue == null){
+        return true;
+      }
+      return initialValue;
+
+  });;
+
+  const [theme, setTheme] = useState(() => {
+    // getting stored value
+    const saved = localStorage.getItem("theme");
+    const initialValue = JSON.parse(saved);
+    return initialValue || 'first';
+  });;
+
+
+  useEffect(() => {
+    // storing last used theme
+    localStorage.setItem('theme', JSON.stringify(theme));
+  }, [theme]);
+
+ useEffect(() => {
+    // storing last used switch
+    localStorage.setItem('checked', JSON.stringify(checked));
+  }, [checked]);
+
 
   const handleDell = () => {
     let newNumber = '';
@@ -21,333 +51,66 @@ function App() {
     
     setNumber(newNumber);
   }
- 
+
+  function parse(str) {
+    return Function(`'use strict'; return (${str})`)()
+  }
 
   const calcMath = () => {
 
-  let result = '';
+     let inputStack = number;
 
-  let inputStack = number;
-  let inputPart =  '';
-
-  let number1 = '';
-  let numberFirstIndex = '';
-
-  let number2 = '';
-  let numberSecondIndex = '0';
-
-  let operatorIndex = 0;
-  let operationCounter = 0;
-
-  let number3 = '';
-  let numberThirdIndex = 0;
-
-  let alphabet = ['a','á','é','í', 'ű', 'ó', 'ü','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w',,'y','z'];
-
- console.log("Egyenlet: " + inputStack);
-
- while(isNaN(inputStack)){
-   
-  if((inputStack.toString().includes('NaN')) === true){
-    setNumber('Malformed expression');
-    return;
-  }
-
-
-  for(let i=0; i<inputStack.length; i++){
-      
-  if(inputStack.includes('x')){
-
-    if(inputStack.includes('xx') ){
-      setNumber('Malformed expression');
-      return;
-    } 
-
-        if(inputStack[i]=='x'){
-          console.log("Find x operator at " + i + " index");
-          operatorIndex = i;
-          
-          //  SELECT FIRST NUMBER
-          for(let j=operatorIndex-1; j>=0; j--){
-             console.log("First number searching: " + inputStack[j]);
-            if((inputStack[j] == 'x' || inputStack[j] == '/' || inputStack[j] == '' || inputStack[j] == '+')){
-              numberFirstIndex = j;
-              break
-            } else if(inputStack[j] == '-'){
-              numberFirstIndex = j;
-            }
-
-              numberFirstIndex = j;
-              
-            
-            
-          }
-
-          number1 = inputStack.slice(numberFirstIndex, operatorIndex);
-          number1 = parseFloat(number1);
-          console.log("First number '*' : " + number1);
-          
-
-          //  SELECT SECOND NUMBER
-          for(let k=operatorIndex+1; k<=inputStack.length-1; k++){
-            if((inputStack[k] == 'x' || inputStack[k] == '/' || inputStack[k] == '' || inputStack[k] == '+') && operationCounter == 0){
-                numberSecondIndex = k-1;
-                operationCounter++;
-            } else numberSecondIndex = k;}
-          
-          
-          number2 = inputStack.slice(operatorIndex+1, numberSecondIndex+1);
-          number2 = parseFloat(number2);
-
-          console.log("Second number '*' : " + number2);
-          number3 = number1 * number2;
-
-
-          //console.log("Szeletelés előtt: " + inputStack);
-          inputPart = (number1 + 'x' + number2);
-          inputStack = inputStack.replace(inputPart, number3);
-          console.log("Szeletelés után: " + inputStack);
-          console.log("Number 3 érték: "+number3);
-          // Setting everything back to null
-          operationCounter = 0;
-          operatorIndex = '';
-          number1 = '';
-          numberFirstIndex = '';
-          number2 = '';
-          numberSecondIndex = '';
-    } 
-  }
-
-
-  if(inputStack.includes('/')){
-
-    if(inputStack.includes('//') ){
-      setNumber('Malformed expression');
-      return
-    } 
-
-    if(inputStack[i]=='/'){
-      console.log("Find / operator at " + i + " index");
-      operatorIndex = i;
-      
-      //  SELECT FIRST NUMBER
-      for(let j=operatorIndex-1; j>=0; j--){
-             
-        if((inputStack[j] == 'x' || inputStack[j] == '/' || inputStack[j] == '-' || inputStack[j] == '+')){
-          numberFirstIndex = j;
-        } numberFirstIndex = j;
-          
-        
-        
-      }
-
-      number1 = inputStack.slice(numberFirstIndex, operatorIndex);
-      number1 = parseFloat(number1);
-      console.log("First number '/' : " + number1);
-
-
-      //  SELECT SECOND NUMBER
-      for(let k=operatorIndex+1; k<=inputStack.length-1; k++){
-        if((inputStack[k] == 'x' || inputStack[k] == '/' || inputStack[k] == '' || inputStack[k] == '+') && operationCounter == 0){
-            numberSecondIndex = k-1;
-            operationCounter++;
-        } else numberSecondIndex = k;}
-      
-     
-      
-   
-
-      if(operatorIndex !== 0){
-        number2 = inputStack.slice(operatorIndex+1, numberSecondIndex+1);
-        number2 = parseFloat(number2);
-      } else {
-        number1 = inputStack.slice(operatorIndex, numberSecondIndex+1);
-      }
-
-     
-      console.log("Second number '/' : " + number2);
-
-      number3 = (number1 / number2);
-
-
-      //console.log("Szeletelés előtt: " + inputStack);
-      inputPart = (number1 + '/' + number2);
-      inputStack = inputStack.replace(inputPart, number3);
-      console.log("Szeletelés után: " + inputStack);
-      console.log("Number 3 érték: "+number3);
-      // Setting everything back to null
-      operationCounter = 0;
-      operatorIndex = '';
-      number1 = '';
-      numberFirstIndex = '';
-      number2 = '';
-      numberSecondIndex = '';
-
-      
-
-    } 
-  
-}
-}
-  for(let i=0; i<inputStack.length; i++){
-  
-    if(inputStack.includes('++') ){
-      setNumber('Malformed expression');
-      return
-    }
-
-    if(inputStack[i]=='+'){
-      console.log("Find + operator at " + i + " index");
-      operatorIndex = i;
-      
-      //  SELECT FIRST NUMBER
-      for(let j=operatorIndex; j>=0; j--){
-        if(inputStack[j] == 'x' || inputStack[j] == '/' || inputStack[j] == '' || inputStack[j] == '+'){
-          numberFirstIndex = j+1;
-        } else numberFirstIndex = j;
-      }
-      
-        number1 = inputStack.slice(numberFirstIndex, operatorIndex);
-        number1 = parseFloat(number1);
-     
-      console.log("First number '+' : " + number1);
-     
-
-      //  SELECT SECOND NUMBER
-      for(let k=operatorIndex+1; k<=inputStack.length-1; k++){
-        if((inputStack[k] == 'x' || inputStack[k] == '/' || inputStack[k] == '' || inputStack[k] == '+') && operationCounter == 0){
-            numberSecondIndex = k-1;
-            operationCounter++;
-        } else numberSecondIndex = k;}
-      
-     if(operatorIndex !== 0){
-      number2 = inputStack.slice(operatorIndex+1, numberSecondIndex+1);
-      number2 = parseFloat(number2);
-    } else {
-      number1 = inputStack.slice(operatorIndex, numberSecondIndex+1);
-    }
-      console.log("Second number '+' : " + number2);
-
-      
-      number3 = (number1 + number2);
-
-      //console.log("Szeletelés előtt: " + inputStack);
-      inputPart = (number1 + '+' + number2);
-      inputStack = inputStack.replace(inputPart, number3);
-      console.log("Szeletelés után: " + inputStack);
-      console.log("Number 3 érték: "+number3);
-      // Setting everything back to null
-      operationCounter = 0;
-      operatorIndex = '';
-      number1 = '';
-      numberFirstIndex = '';
-      number2 = '';
-      numberSecondIndex = '';
-
-      }
-    
-
-    if(inputStack[i]=='-'){
-      console.log("Find - operator at " + i + " index");
-      operatorIndex = i;
-      
-      //  SELECT FIRST NUMBER
-      for(let j=operatorIndex; j>=0; j--){
-        if(inputStack[j] == 'x' || inputStack[j] == '/' || inputStack[j] == '' || inputStack[j] == '+'){
-          numberFirstIndex = j+1;
-        } else numberFirstIndex = j;
-      }
-
-      number1 = inputStack.slice(numberFirstIndex, operatorIndex);
-      number1 = parseFloat(number1);
-      console.log("First number '-' : " + number1);
-     
-
-      //  SELECT SECOND NUMBER
-      for(let k=operatorIndex+1; k<=inputStack.length-1; k++){
-        if((inputStack[k] == 'x' || inputStack[k] == '/' || inputStack[k] == '' || inputStack[k] == '+') && operationCounter == 0){
-            numberSecondIndex = k-1;
-            operationCounter++;
-        } else numberSecondIndex = k;}
-      
-        if(operatorIndex !== 0){
-          number2 = inputStack.slice(operatorIndex+1, numberSecondIndex+1);
-          number2 = parseFloat(number2);
-        } else {
-          number1 = inputStack.slice(operatorIndex, numberSecondIndex+1);
-        }
-
-     
-      console.log("Second number '-' : " + number2);
-
-      if(inputStack=== '--'){
+      if(inputStack.includes('x') || inputStack.includes('++') || inputStack.includes('//') || inputStack.includes('..') || inputStack === '--' || inputStack === '+' 
+      || inputStack === '*' || inputStack === '/' || inputStack === '-' || inputStack === '.'){
         setNumber('Malformed expression');
-        return
-      } 
-      else {
-      number3 = (number1 - number2);
-      }
+      } else {
 
-      //console.log("Szeletelés előtt: " + inputStack);
-      inputPart = (number1 + '-' + number2);
-      inputStack = inputStack.replace(inputPart, number3);
-      console.log("Szeletelés után: " + inputStack);
-      console.log("Number 3 érték: "+number3);
-      // Setting everything back to null
-      operationCounter = 0;
-      operatorIndex = '';
-      number1 = '';
-      numberFirstIndex = '';
-      number2 = '';
-      numberSecondIndex = '';
-
-      }
-    
-  }
-
-
-
-    //if(isNaN(number3)){
-    //  break;
-   // }
-    
-   // inputStack = parseFloat(inputStack);
-   
-   //// if((inputStack.toString().includes(alphabet)) === true){
-   //   setNumber('Malformed expression');
-   //   return;
-  //  }
-
- }
-  result = number3; 
-  console.log('Result: '+ result);
-  console.log('***************************');
-  //setNumber(result);
+     let result = parse(inputStack);
+     setNumber(result);
+    }
 }
-
-
-
-
-
-
-
-
 
 
 
 
   return (
-    <div className="App">
+    
+    <div className="App" data-theme={theme}>
         <div className='calc-layout'>
           {/* TITLE + SWITCH */}
           <div className='title-theme'>
               <div className='calc-title'>calc</div>
                 <div className='theme-switch'>
-                  <div>
+                  <div className='themeTitle'>
                     theme
                   </div>
-                  <div>
-                    input switch
+                  <div className='radioContainer'>
+
+                    <input type="radio" id={`firstTheme`} value={1} name='tss'
+                    onChange={(e) => setChecked(e.target.checked)}
+                    onClick={() => setTheme('first')}
+                    />
+                    <label
+                      className="firstTheme"
+                      htmlFor={`firstTheme`}
+                    >1</label>
+                     <input type="radio" id={`secondTheme`} value={2} name='tss'
+                     onChange={(e) => setChecked(e.target.checked)}
+                     onClick={() => setTheme('second')}
+                     />
+                    <label
+                      className="secondTheme"
+                      htmlFor={`secondTheme`}
+                    >2</label>
+                     <input type="radio" id={`thirdTheme`} value={3} name='tss'
+                     onChange={(e) => setChecked(e.target.checked)}
+                     onClick={() => setTheme('third')}
+                     
+                     />
+                    <label
+                      className="thirdTheme"
+                      htmlFor={`thirdTheme`}
+                    >3</label>
                   </div>
                 </div>
           </div>
@@ -421,7 +184,7 @@ function App() {
              onClick={() => setNumber(number+'/')}
              >/</div>
              <div className='num-btn'
-             onClick={() => setNumber(number+'x')}
+             onClick={() => setNumber(number+'*')}
              >x</div>
           
              </div>
@@ -432,6 +195,8 @@ function App() {
             >Reset</button>
             <button className='eq-btn'
             onClick={calcMath}
+            
+           
             >=</button>
           </div>
 
